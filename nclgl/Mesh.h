2daba2010a7acd5618e84caa6d1acaed0bf1c987
TERRAN10\Admin <bensbow@hotmail.com>
2017-10-30 16:40:10 +0000

    initial commit

---
+++ nclgl/Mesh.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "OGLRenderer.h"
+
+enum MeshBuffer {
+	VERTEX_BUFFER, COLOUR_BUFFER, TEXTURE_BUFFER, MAX_BUFFER};
+class Mesh {
+public:
+	Mesh(void);
+	~Mesh(void);
+
+	virtual void Draw();
+	static Mesh * GenerateTriangle();
+
+	void SetTexture(GLuint tex) { texture = tex; }
+	GLuint GetTexture() { return texture; }
+
+protected:
+	void BufferData();
+
+	GLuint arrayObject;
+	GLuint bufferObject[MAX_BUFFER];
+	GLuint numVertices;
+	GLuint type;
+
+	Vector3 * vertices;
+	Vector4 * colours;
+
+	GLuint texture;
+	Vector2 * textureCoords;
+
+};
